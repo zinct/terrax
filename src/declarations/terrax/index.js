42400ded7adbd0814c16de287af6eb00b6fb8bb5
Indra Mahesa <indramahesa128@gmail.com>
2024-01-14 22:14:15 +0700

    feat: update declaration

--- src/declarations/terrax/index.js
+++ src/declarations/terrax/index.js
@@ -10,8 +10,7 @@ export { idlFactory } from "./terrax.did.js";
  * beginning in dfx 0.15.0
  */
 export const canisterId =
-  process.env.CANISTER_ID_TERRAX ||
-  process.env.TERRAX_CANISTER_ID;
+  process.env.CANISTER_ID_TERRAX || process.env.NEXT_PUBLIC_TERRAX_CANISTER_ID;
 
 export const createActor = (canisterId, options = {}) => {
   const agent = options.agent || new HttpAgent({ ...options.agentOptions });
